730003576e9d07966e7d28b1db9ebef1dca3709e
Angel Herrarte <aherrarte-2019037@kinal.edu.gt>
2021-07-19 19:56:31 -0600

    Se cambio el controlador de usuario

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,5 +7,6 @@ var api = express.Router()
 
 api.post('/login', userController.login);
 api.post('/register', userController.register);
+api.post('/login/social', userController.socialLogin);
 
 module.exports = api;
