2731069e070232a8a15acc469b289d9ecf6259e4
Sergio Mayen <sergiomayen316@gmail.com>
2021-07-22 14:53:55 -0600

    Se creó funcion like y unlike

--- src/controllers/recipe.controllers.js
+++ src/controllers/recipe.controllers.js
@@ -4,78 +4,125 @@ const recipeModel = require('../models/recipe.model');
 const Recipe = require('../models/recipe.model')
 const User = require('../models/user.model');
 
-function createRecipe(req, res){
+function createRecipe(req, res) {
 
     var params = req.body;
 
-    if(params.name && params.description && params.category){
+    if (params.name && params.description && params.category) {
 
-        var recipeModel = new Recipe({...params});
+        var recipeModel = new Recipe({ ...params });
 
-        if(req.user.rol == 'chef'){
+        if (req.user.rol == 'chef') {
             recipeModel.type = 'premium';
-        }else{
+        } else {
             recipeModel.type = 'common'
         }
 
         recipeModel.dateTime = new Date(Date.now())
         recipeModel.idPublisher = req.user.sub;
 
-        recipeModel.save((err,savedRecipe)=>{
-            if (err) return res.status(500).send({message: 'Error en la petición', err});
-            if (!savedRecipe) return res.status(500).send({message: 'Error al guardar la receta'});
+        recipeModel.save((err, savedRecipe) => {
+            if (err) return res.status(500).send({ message: 'Error en la petición', err });
+            if (!savedRecipe) return res.status(500).send({ message: 'Error al guardar la receta' });
 
-            User.findByIdAndUpdate(req.user.sub, { $inc: { ezCoins: +5 } }, {new: true, useNewUrlParser: false}, (err, ezCoinsAdded) => {
-                if(err) return res.status(500).send({message: 'Error en la petición'})
+            User.findByIdAndUpdate(req.user.sub, { $inc: { ezCoins: +5 } }, { new: true, useNewUrlParser: false }, (err, ezCoinsAdded) => {
+                if (err) return res.status(500).send({ message: 'Error en la petición' })
             })
-            
-            return res.status(200).send({message: 'Se agregó la receta',savedRecipe});
+
+            return res.status(200).send({ message: 'Se agregó la receta', savedRecipe });
 
         })
-    }else{
+    } else {
 
-        return res.status(500).send({message: 'Debe llenar todos los datos'});
+        return res.status(500).send({ message: 'Debe llenar todos los datos' });
 
     }
 
 }
 
 
-function getRecipe(req, res){
-    Recipe.find((err, foundRecipes) =>{
-        if(err) return res.status(500).send({ message: 'Error en la petición'});
-        if(!foundRecipes) return res.status(500).send({ message: 'Error al traer las Recetas'});
+function getRecipe(req, res) {
+    Recipe.find((err, foundRecipes) => {
+        if (err) return res.status(500).send({ message: 'Error en la petición' });
+        if (!foundRecipes) return res.status(500).send({ message: 'Error al traer las Recetas' });
 
-        return res.status(200).send({foundRecipes});
+        return res.status(200).send({ foundRecipes });
     })
 }
 
 
-function getMyRecipes(req, res){
+function getMyRecipes(req, res) {
     var userId = req.user.sub;
-    Recipe.find({idPublisher: userId}, (err, foundMyRecipes)=>{
-        if(err) return res.status(500).send({ message: 'Error en la petición'});
-        if(!foundMyRecipes) return res.status(500).send({ message: 'Error al traer las Recetas'});
+    Recipe.find({ idPublisher: userId }, (err, foundMyRecipes) => {
+        if (err) return res.status(500).send({ message: 'Error en la petición' });
+        if (!foundMyRecipes) return res.status(500).send({ message: 'Error al traer las Recetas' });
 
-        return res.status(200).send({foundMyRecipes});
+        return res.status(200).send({ foundMyRecipes });
     })
 }
 
-async function latestRecipes(req,res){
+async function latestRecipes(req, res) {
     try {
-        const normal = await Recipe.find({type: 'common'}).limit(2).sort({dateTime:-1})
+        const normal = await Recipe.find({ type: 'common' }).limit(2).sort({ dateTime: -1 })
 
-        const premium = await Recipe.find({type: 'premium'}).limit(2).sort({dateTime:-1})
-    
+        const premium = await Recipe.find({ type: 'premium' }).limit(2).sort({ dateTime: -1 })
+
         return res.status(200).send(normal.concat(premium))
     } catch (error) {
-        return res.status(500).send({error})
+        return res.status(500).send({ error })
     }
 }
 
-module.exports={
+function giveLikes(req, res) {
+    var idRecipe = req.params.idRecipe;
+    var cont = 0;
+    Recipe.findById(idRecipe, (err, foundRecipes) => {
+        if (err) return res.status(500).send({ massage: 'Error al buscar la receta' })
+        if (!foundRecipes) return res.status(500).send({ massage: 'Error al retornar la receta' })
+        for (let i = 0; i < foundRecipes.likes.length; i++) {
+
+            if (foundRecipes.likes[i].toString() === req.user.sub) {
+                cont++
+            }
+        }
+        if (cont === 1) return res.status(200).send({ message: "Ya le diste like a esta publicación" })
+
+        Recipe.findByIdAndUpdate(idRecipe, {$push:{likes: req.user.sub}}, (err, foundRecipes) => {
+            if (err) return res.status(500).send({ massage: 'error al actualizar la receta' })
+            if (!foundRecipes) return res.status(500).send({ message: 'Error con encontrar la receta' })
+        })
+        return res.status(200).send({ menssage: 'Te a gustado la publicacion' })
+    })
+}
+
+function unlike(req, res    ){
+    var idRecipe = req.params.idRecipe;
+    var cont = 0 
+
+    Recipe.findById(idRecipe, (err, foundRecipes) => {
+        if (err) return res.status(500).send({ massage: 'Error al buscar la receta' })
+        if (!foundRecipes) return res.status(500).send({ massage: 'Error al retornar la receta' })
+        for (let i = 0; i < foundRecipes.likes.length; i++) {
+
+            if (foundRecipes.likes[i].toString() === req.user.sub) {
+                cont++
+            }
+        }
+        if (cont === 0) return res.status(200).send({ message: "Ya no te gusta la publicación" })
+
+        Recipe.findByIdAndUpdate(idRecipe, {$pull: {likes: req.user.sub}}, (err, unlikedPost) => {
+            if (err) return res.status(500).send({ massage: 'error al actualizar la receta' })
+            return res.status(200).send({ menssage: 'Ya no te gusta la publicacion', unlikedPost })
+
+        })
+    })
+}
+
+module.exports = {
     createRecipe,
     getRecipe,
     getMyRecipes,
-    latestRecipes
+    latestRecipes,
+    giveLikes,
+    unlike
 }
