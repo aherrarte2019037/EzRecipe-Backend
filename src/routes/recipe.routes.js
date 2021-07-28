a87e7201370d478db396844f3fababc69ad0807c
José Góngora <jdavidgongorao@gmail.com>
2021-07-28 17:12:45 -0600

    Se editó recipe controller

--- src/routes/recipe.routes.js
+++ src/routes/recipe.routes.js
@@ -10,7 +10,7 @@ api.post('/addRecipe',md_authentication.ensureAuth,recipeController.createRecipe
 api.get('/getRecipe',md_authentication.ensureAuth,recipeController.getRecipe);
 api.get('/getMyRecipes', md_authentication.ensureAuth, recipeController.getMyRecipes);
 api.get('/latestRecipes', md_authentication.ensureAuth, recipeController.latestRecipes);
-api.put('/giveLikes/:idRecipe', md_authentication.ensureAuth, recipeController.giveLikes);
+api.get('/giveLikes/:idRecipe', md_authentication.ensureAuth, recipeController.giveLikes);
 api.get('/getRecipesIdPublisher/:userId', md_authentication.ensureAuth, recipeController.getRecipesIdPublisher);
 api.put('/saveRecipe/:idRecipe', md_authentication.ensureAuth, recipeController.saveRecipe)
 
