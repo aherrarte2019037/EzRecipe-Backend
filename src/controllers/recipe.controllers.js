ef051b1cfba7bc3d144b7c868eedcadf5d882f0a
José Góngora <jdavidgongorao@gmail.com>
2021-07-28 19:39:00 -0600

    Se hicieron algunos cambios

--- src/controllers/recipe.controllers.js
+++ src/controllers/recipe.controllers.js
@@ -98,13 +98,13 @@ function giveLikes(req, res) {
         if (cont === 1){
             Recipe.findByIdAndUpdate(idRecipe, {$pull: {likes: req.user.sub}}, (err, unlikedPost) => {
                 if (err) return res.status(500).send({ massage: 'error al actualizar la receta' })
-                return res.status(200).send({ menssage: 'Ya no te gusta la publicacion' })
+                return res.status(200).send({ menssage: 'Ya no te gusta la publicacion', user: req.user.sub })
             })
         }else {
             Recipe.findByIdAndUpdate(idRecipe, {$push:{likes: req.user.sub}}, (err, foundRecipes) => {
                 if (err) return res.status(500).send({ massage: 'error al actualizar la receta' })
                 if (!foundRecipes) return res.status(500).send({ message: 'Error con encontrar la receta' })
-                return res.status(200).send({ menssage: 'Te a gustado la publicacion' })
+                return res.status(200).send({ menssage: 'Te a gustado la publicacion', user: req.user.sub })
             })
         }
         
